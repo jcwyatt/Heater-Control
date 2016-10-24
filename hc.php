2b03091b61b87f7e60e54ec55fda1a99412c03ff
jcwyatt <jwyatt043@googlemail.com>
2016-10-24 21:07:05 +0100

    Updated html on homepage to improve design. PHP action page hc.php created.

---
+++ hc.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" type="text/css" href="heat.css">
+	<title>Home Heating Control Scheduler</title>
+
+</head>
+
+<body>
+
+<h1>Heater Control Settings</h1>
+
+
+<?php
+<h2>Your settings:</h2>
+
+<p>Target Temp: echo $_POST["target"];
+
+<p> On: echo $_POST["target"] Off: echo $_POST["target"];
+<p> On: echo $_POST["target"] Off: echo $_POST["target"];
+
+<h2>Overides</h2>
+<p>Sleep (all off): echo $_POST["target"];
+<p>Manual Overide:  echo $_POST["target"];
+?>
+
+</body>
+</html>
